4a874ef174b9eeb38d1613de55ce5d4185f533ba
Starlog <785276471@qq.com>
2020-10-16 16:32:24 +0800

    修改一些配置，部署上线

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, Suspense } from 'react'
 import { renderRoutes } from "react-router-config"
 import { HashRouter } from "react-router-dom"
 import { Provider } from "react-redux";
@@ -15,7 +15,9 @@ export default memo(function App() {
     <Provider store={store}>
       <HashRouter>
         <ZCAppHeader/>
-          {renderRoutes(routes)}
+          <Suspense fallback={<div>loading...</div>}>
+            {renderRoutes(routes)}
+          </Suspense>
         <ZCAppFooter/>
         <ZCAppPlayBar/>
       </HashRouter>
--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 const devBaseURL = "http://120.53.125.40:3000/";
-const proBaseURL = "https://production.org";
+const proBaseURL = "http://120.53.125.40:3000/";
 export const BASEURL = process.env.NODE_ENV === 'development' ? devBaseURL: proBaseURL;
 
 export const TIMEOUT = 5000;
